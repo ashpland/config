451834d46ef6301b37814c7c3beaccc67578634e
Andrew Shopland <hi@ashp.land>
2019-01-14 13:07:38 -0800

    Terminal Theme

---
+++ terminal-theme.js
@@ -0,0 +1,26 @@
+black       = "#404040";
+red         = "#EBEBEB"; 
+green       = "#333333"; 
+yellow      = "#787878"; 
+blue        = "#939393"; 
+magenta     = "#FFFFFF"; 
+cyan        = "#000000"; 
+white       = "#AAAAAA"; 
+lightBlack  = "#000000";
+lightRed    = "#EBEBEB"; 
+lightGreen  = "#333333"; 
+lightYellow = "#797979";
+lightBlue   = "#9E9E9E"; 
+lightMagenta= "#FFFFFF"; 
+lightCyan   = "#000000"; 
+lightWhite  = "#A9A9A9"; 
+
+t.prefs_.set('color-palette-overrides',
+    [ black , red     , green  , yellow,
+        blue     , magenta , cyan   , white,
+        lightBlack   , lightRed  , lightGreen , lightYellow,
+        lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(146, 146, 146, 0.5)');
+t.prefs_.set('foreground-color', "#B5B5B5");
+t.prefs_.set('background-color', "#5F5F5F");
